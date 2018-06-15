ce095af42496f6e3fe1988d6bb72121fd9fec961
Lucas <the-lucas01@live.com>
2018-06-14 22:39:57 -0300

    Modulo 8 , programação funcional.

---
+++ modulo8.js
@@ -0,0 +1,15 @@
+// functions .
+
+var func = function func (){
+    return 'i'
+}
+
+func();
+//retorna i.
+
+x.func();
+//chama a funcao .
+
+
+// programação funcional.
+// programar utilizando funcoes.
